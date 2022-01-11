1aced75a5c21dfd58d16075c6c44f774b256df25
micro <damianohart120905@gmail.com>
2022-01-11 12:47:48 -0500

    Update commands.js

--- src/utils/commands.js
+++ src/utils/commands.js
@@ -7,13 +7,15 @@ const Discord = require("discord.js");
  * @param {Discord.Client} client The Discord client.
  */
 module.exports = (client) => {
+    client.commands = Discord.Collection();
+
     const COMMAND_FILES = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js"));
 
     const COMMANDS = COMMAND_FILES.map(file => require(`../commands/${file}`));
 
-    client.commands = COMMANDS
+    COMMANDS
         .filter(command => ["BOTH", "TEXT"].includes(command.type))
-        .forEach(command => require(`../commands/${command.name}`));
+        .forEach(command => client.commands.set(command.name, require(`../commands/${command.name}`)));
 
     client.slashCommands = COMMANDS
         .filter(command => ["BOTH", "SLASH"].includes(command.type))
