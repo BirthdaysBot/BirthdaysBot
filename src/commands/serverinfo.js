2a7520db95b76ca798cd29ccc73a472b4eaadd00
micro <damianohart120905@gmail.com>
2022-01-16 14:57:29 -0500

    Sharding + new commands + minor changes

---
+++ src/commands/serverinfo.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "serverinfo",
+    description: "Fetches some information about the server.",
+    adminOnly: false,
+    type: "BOTH",
+    slashCommandOptions: [],
+    run: async (client, message, args) => {
+        const embed = new MessageEmbed();
+
+        embed.setTitle(`${message.guild.name}`);
+        embed.setDescription(`Owned by ${(async () => await message.guild.fetchOwner(member => { return member.tag }))()}`);
+        embed.addFields([
+            {
+                name: "Shard",
+                value: message.guild.shardId.toLocaleString(),
+                inline: true
+            },
+            {
+                name: "Members",
+                value: `${message.guild.memberCount.toLocaleString()}: Users: ${message.guild.members.cache.filter(member => !member.bot).size.toLocaleString()} Bots: ${message.guild.members.cache.filter(member => member.bot).size.toLocaleString()}`,
+                inline: false
+            }
+        ]);
+        embed.setTimestamp();
+
+        message.reply({ embeds: [embed] });
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-const Discord = require("discord.js");
-const intents = new Discord.Intents(513);
-const Client = new Discord.Client({ intents });
+const { ShardingManager } = require("discord.js");
 
-Client.globalConfig = require("./config.json");
+const Config = require("./config.json");
 
-require("./utils/commands")(Client);
-require("./utils/events")(Client);
+const Manager = new ShardingManager("./src/bot.js", { token: Config.BOT_TOKEN, totalShards: 1 });
 
-Client.login(Client.globalConfig.BOT_TOKEN);
+Manager.on("shardCreate", (shard) => {
+    console.log(`[SHARD - ${shard.id}] Launched`);
+
+    shard.on("ready", () => {
+        console.log(`[SHARD - ${shard.id}] Ready`);
+    });
+
+    shard.on("error", (error) => {
+        console.log(`[SHARD - ${shard.id}] Error: ${error}`);
+    });
+
+    shard.on("disconnect", () => {
+        console.log(`[SHARD - ${shard.id}] Disconnected`);
+    });
+});
+
+Manager.spawn();
