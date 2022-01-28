092f97946e8a46562e8ce0806d509f43686fc454
micro <damianohart120905@gmail.com>
2022-01-27 21:56:10 -0500

    Create Command.js

---
+++ src/utils/structures/Command.js
@@ -0,0 +1,29 @@
+const { Client, Message, ApplicationCommandOption } = require("discord.js");
+
+/**
+ * The run function for a bot command.
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {Array} args 
+ */
+function runFunction(client, message, args) { }
+
+class Command {
+    /**
+     * Makes a new command.
+     * @typedef {"BOTH" | "SLASH" | "TEXT"} CommandType
+     * @typedef {{name: String, description: String, adminOnly: Boolean, ownerOnly: Boolean, type: CommandType, slashCommandOptions: ApplicationCommandOption[], run: runFunction}} CommandOptions
+     * @param {CommandOptions} options The options for the command.
+     */
+    constructor(options) {
+        this.name = options.name;
+        this.description = options.description;
+        this.adminOnly = options.adminOnly;
+        this.ownerOnly = options.ownerOnly;
+        this.type = ["BOTH", "SLASH", "TEXT"].includes(options.type) ? options.type : "TEXT";
+        this.slashCommandOptions = options.slashCommandOptions;
+        this.run = options.run;
+    }
+}
+
+module.exports = Command;
